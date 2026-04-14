a131287447db6a6c37e9630d1690a00d4cf4ad40
wngml02 <wngml021211@gmail.com>
2026-04-14 19:07:19 +0900

    [chore, feat] 버킷 기능 파일 생성

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-import express from 'express';
 import cookieParser from 'cookie-parser';
+import express from 'express';
+import { errorMiddleware } from './middlewares/errorMiddleware.js';
+import bucketRoute from './routes/bucketRoute.js';
 import testRoute from './routes/testRoute.js';
 
 const app = express();
@@ -8,5 +10,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api', testRoute);
-
+app.use('/api/v1/buckets', bucketRoute);
+app.use(errorMiddleware);
 export default app;
